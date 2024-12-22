64c48f0b00d34fb9a83be90d3ceddeba36b579bb
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2024-12-22 18:55:29 +0530

    replacing Win1252 symbols with ascii chars

--- 16removediacritclatin.py
+++ 16removediacritclatin.py
@@ -17,3 +17,28 @@ def shave_marks_latin(txt):
 text = "Café São Paulo, mañana, ありがとう"
 shaved_text = shave_marks_latin(text)
 print(shaved_text)
+
+single_map = str.maketrans("""‚ƒ„†ˆ‹‘’“”•–—˜›""", 
+ """'f"*^<''""---~>""")
+multi_map = str.maketrans({ 
+ '€': '<euro>',
+ '…': '...',
+ 'Œ': 'OE',
+ '™': '(TM)',
+ 'œ': 'oe',
+ '‰': '<per mille>',
+ '‡': '**',
+})
+multi_map.update(single_map) 
+def dewinize(txt):
+ """Replace Win1252 symbols with ASCII chars or sequences"""
+ return txt.translate(multi_map) 
+def asciize(txt):
+ no_marks = shave_marks_latin(dewinize(txt)) 
+ no_marks = no_marks.replace('ß', 'ss') 
+ return unicodedata.normalize('NFKC', no_marks)
+order = '“Herr Voß: • ½ cup of Œtker™ caffè latte • bowl of açaí.”'
+print(dewinize(order))
+#'"Herr Voß: - ½ cup of OEtker(TM) caffè latte - bowl of açaí."' 
+print(asciize(order))
+#'"Herr Voss: - 1⁄2 c
