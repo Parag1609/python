39772b050a380b31570926480adcb9cf36a2b392
Parag Jain <142827074+Parag1609@users.noreply.github.com>
2025-01-26 16:40:52 +0530

    parameterized decorator

---
+++ 29parameterizedDecorator.py
@@ -0,0 +1,19 @@
+registry = set()
+def register(active = True):
+    def decorator(func):
+        print('running register(active=%s)->decorate(%s)'% (active, func))
+        if(active):
+            registry.add(func)
+        else:
+            registry.discard(func)
+        return func
+    return decorator
+
+@register(active=False) 
+def f1():
+ print('running f1()')
+@register() 
+def f2():
+ print('running f2()')
+def f3():
+ print('running f3()')
